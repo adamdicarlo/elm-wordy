6ef9fdc3c16a9c2feeba27f36626e01b7029f032
Adam DiCarlo <97462+adamdicarlo@users.noreply.github.com>
2017-02-07 20:56:22 -0800

    :sparkles: Use create-elm-app

--- build.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-elm-make --yes src/Main.elm --output=dist/wordy.elm.js
-google-closure-compiler-js --compilationLevel ADVANCED --languageIn ES5 dist/wordy.elm.js > dist/wordy.elm.min.js
-mv dist/wordy.elm.min.js dist/wordy.elm.js
---
+++ src/index.js
@@ -0,0 +1,3 @@
+require('./main.css')
+var Elm = require('./Main.elm')
+Elm.Main.embed(document.getElementById('root'))
